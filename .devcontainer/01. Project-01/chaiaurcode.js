65ae5c2e2b0c2dda13d026878efe345e05ef5406
Shaid Islam <66918424+shaid85@users.noreply.github.com>
2023-11-16 07:53:06 +0000

    Add files for color chagne by button

---
+++ .devcontainer/01. Project-01/chaiaurcode.js
@@ -0,0 +1,12 @@
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body')
+
+button.forEach(function (btndata) {
+  console.log(btndata.id);
+  btndata.addEventListener('click', function(e){
+    if(e.target.id == 'grey'){body.style.backgroundColor = e.target.id}
+    if(e.target.id == 'red'){body.style.backgroundColor = e.target.id}
+    if(e.target.id == 'blue'){body.style.backgroundColor = e.target.id}
+    if(e.target.id == 'yellow'){body.style.backgroundColor = e.target.id}
+  })
+});
